257eb30d319f2f1c8ab960ee77c48aba5cd5b5cb
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2024-01-30 18:23:06 +0000

    v0.6.2 - Fix Version spacing

--- components/atoms/Version/Version.tsx
+++ components/atoms/Version/Version.tsx
@@ -2,7 +2,7 @@ import Package from "../../../package.json";
 
 const Version = () => {
   return (
-    <div className="flex bg-slate justify-center w-full text-sm text-gray">
+    <div className="flex bg-slate justify-center w-full text-sm text-gray pt-4">
       <span>v{Package.version}</span>
     </div>
   );
